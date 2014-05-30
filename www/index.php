de180dcd97e12d9d25756f39406f6c118ccc2c8c
antonio68 <antonio.stojanovic@troxo.com>
2014-05-30 22:25:41 +0200

    config changes
    config changes

--- www/index.php
+++ www/index.php
@@ -1,13 +1,19 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../../frameworks/YiiFramework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+if (YII_DEBUG) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    ini_set('log_errors', '0');
+}
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
